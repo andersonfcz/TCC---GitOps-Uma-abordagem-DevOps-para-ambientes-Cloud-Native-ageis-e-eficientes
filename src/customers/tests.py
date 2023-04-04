2f6f3e4c1b670c07a72ef124866d1be105d24482
Anderson <andersonfcz@bol.com.br>
2023-04-03 22:31:53 -0300

    add unit test

--- src/customers/tests.py
+++ src/customers/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from models import Customer
 
 # Create your tests here.
+
+class CustomerModelTestCase(TestCase):
+    def setUp(self):
+        self.customer = Customer.objects.create(
+            name='John Doe',
+            address='123 Main St',
+            phone='555-1234',
+            email='john.doe@example.com',
+            cpf = '123.456.789-10'
+        )
+
+    def test_customer_creation(self):
+        self.assertEqual(str(self.customer), 'John Doe')
+        self.assertEqual(self.customer.name, 'John Doe')
+        self.assertEqual(self.customer.address, '123 Main St')
+        self.assertEqual(self.customer.phone, '555-1234')
+        self.assertEqual(self.customer.email, 'john.doe@example.com')
+        self.assertEqual(self.customer.cpf, '123.456.789-10')
